4521e2a4af672412a62b26de36726198a852234f
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2024-06-06 12:16:08 -0300

    enviar valores do lance

--- leilao.js
+++ leilao.js
@@ -5,15 +5,22 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+ const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(socket.id);
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
